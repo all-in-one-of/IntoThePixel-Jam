def036d45c3b91d400f86f3d0f07a53d9f593b3b
Stella <puddinglaterne@googlemail.com>
2017-03-17 21:17:13 +0100

    Getting Started
    Party Hard

---
+++ TeaTime/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float MoveSpeed;
+
+    private Rigidbody2D rb;
+
+    public void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void FixedUpdate()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(horizontalMovement * MoveSpeed * Time.fixedDeltaTime, rb.velocity.y);
+    }
+
+}
---
+++ TeaTime/Assets/Scripts/Throwing.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throwing : MonoBehaviour
+{
+    public GameObject ObjectToThrow;
+    public Vector2 Offset;
+    public float MaxForce;
+    public AnimationCurve ForceFraction;
+    public float DeadThreshold = 0.2f;
+    public float ThrowThreshold = 1f;
+
+    private float windupStartTime = -1f;
+
+    public void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal2"), Input.GetAxis("Vertical2"));
+        if(windupStartTime < 0)
+        {
+            if(input.magnitude >= DeadThreshold)
+            {
+                windupStartTime = Time.time;
+            }
+        }
+        else if(input.magnitude >= ThrowThreshold)
+        {
+            float force = ForceFraction.Evaluate(Time.time - windupStartTime) * MaxForce;
+            Throw(force, input.normalized);
+        }
+    }
+
+    private void Throw(float force, Vector2 direction)
+    {
+        GameObject projectile = GameObject.Instantiate(ObjectToThrow);
+        projectile.transform.position = (Vector2)transform.position + Offset;
+        projectile.GetComponent<Rigidbody2D>().AddForce(direction * force, ForceMode2D.Impulse);
+        windupStartTime = -1f;
+    }
+}
