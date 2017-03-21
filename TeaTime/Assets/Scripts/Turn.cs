0663a13e21d78f6768d5c105e17ba09c87cbef8a
Stella <puddinglaterne@googlemail.com>
2017-03-21 12:40:41 +0100

    Turntables

---
+++ TeaTime/Assets/Scripts/Turn.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turn : MonoBehaviour
+{
+    public Vector3 Rotation;
+
+    public void Update()
+    {
+        transform.eulerAngles += Rotation * Time.deltaTime;
+    }
+}
